3b63583802d33a1d4d44ed5137e604247170770b
Yehwan Baek <baekokok98@gmail.com>
2023-06-21 16:07:22 +1000

    added code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def show
+        user = find_user
+        render json: user, include: :items
+    end
+
+    private
+    
+    def find_user
+        User.find_by(id: params[:id])
+    end
 end
